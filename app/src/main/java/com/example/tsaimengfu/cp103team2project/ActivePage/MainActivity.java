8a8ae3d4cf209b6ed0f89254f89587ad4c21d757
fred22211269 <fred22211269@gamil.com>
2019-01-17 14:09:55 +0800

    register finish and login qr code finish

--- app/src/main/java/com/example/tsaimengfu/cp103team2project/ActivePage/MainActivity.java
+++ app/src/main/java/com/example/tsaimengfu/cp103team2project/ActivePage/MainActivity.java
@@ -5,23 +5,21 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import com.example.tsaimengfu.cp103team2project.QRCode.qrcode.CreateTeamActivity;
 import com.example.tsaimengfu.cp103team2project.R;
 import com.example.tsaimengfu.cp103team2project.task.Common;
 import com.example.tsaimengfu.cp103team2project.task.CommonTask;
-import com.example.tsaimengfu.cp103team2project.task.MyTask;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
 import com.facebook.FacebookException;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
-import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 public class MainActivity extends AppCompatActivity {
